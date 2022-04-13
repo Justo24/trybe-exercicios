abe61c34df7b4e85c7adad4877b92f6139482def
Nícolas Justo Melão <nitromjusto@gmail.com>
2022-04-13 18:38:21 -0300

    Operações Aritmeticas

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/javascript-primeiros-passos/operacoes-aritmeticas.js
@@ -0,0 +1,18 @@
+const a = 10;
+const b = 15;
+
+if(a + b) {
+    console.log(a + b)
+}
+else if(a - b) {
+    console.log(a - b)
+}
+else if(a * b) {
+    console.log(a * b)
+} 
+else if(a / b) {
+    console.log(a / b)
+}
+else if(a % b) {
+    console.log(a % b)
+}
