db9273958d779c2bcab009449d580957a4740015
Nícolas Justo Melão <nitromjusto@gmail.com>
2022-04-14 16:12:27 -0300

    Javascript array for of

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/javascript-array-e-loop-for/para-fixa/script.js
@@ -0,0 +1,38 @@
+//Exercicio 1
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu;
+
+console.log(menuServices);
+
+//Exercicio 2
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = [2];
+
+console.log(indexOfPortfolio);
+
+//Exercicio 3
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push = ('Contato');
+
+console.log(menu);
+
+//For
+//Exercicio 1
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let i = 0; i < groceryList.length; i+= 1){
+    console.log(groceryList[i])
+}
+
+//For/Of
+//Exercicio 1
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (name of names) {
+    console.log(name)
+}
