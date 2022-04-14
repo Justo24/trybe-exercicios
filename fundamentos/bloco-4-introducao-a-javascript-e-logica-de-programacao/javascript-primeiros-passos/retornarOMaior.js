c9040404fdbaa1e9496b3121f9ac3250ed481981
Nícolas Justo Melão <nitromjusto@gmail.com>
2022-04-13 22:23:03 -0300

    script para retornar o maior numero/ para fazer

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/javascript-primeiros-passos/retornarOMaior.js
@@ -0,0 +1,13 @@
+const a = 100;
+const b = 40;
+const c = 30;
+
+
+if(a > b & c) {
+    console.log(a,"é maior")
+}else if(b > c){
+    console.log(b,"é maior")
+}
+else {
+    console.log(c,"é maior")
+}
